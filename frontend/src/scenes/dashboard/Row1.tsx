1ab6a03bcf8a239fe2f17386e49921606b9b78e2
veeravivekt <veeravivek.telagani@sjsu.edu>
2024-09-04 12:09:19 -0700

    refactor and layout update

---
+++ frontend/src/scenes/dashboard/Row1.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import DashboardBox from '@/components/DashboardBox'
+
+const Row1 = () => {
+  return (
+    <>
+    <DashboardBox gridArea="a"></DashboardBox>
+    <DashboardBox gridArea="b"></DashboardBox>
+    <DashboardBox gridArea="c"></DashboardBox>
+    </>
+  )
+}
+
+export default Row1
--- frontend/src/scenes/dashboard/index.tsx
+++ frontend/src/scenes/dashboard/index.tsx
@@ -1,46 +1,80 @@
-// import { useTheme } from '@mui/material';
-import { Box } from '@mui/material';
-import React from 'react';
+import { Box, useMediaQuery } from "@mui/material";
+import Row1 from "./Row1";
+import Row2 from "./Row2";
+import Row3 from "./Row3";
 
-// eslint-disable-next-line @typescript-eslint/no-empty-object-type
-type Props = {}
+const gridTemplateLargeScreens = `
+  "a b c"
+  "a b c"
+  "a b c"
+  "a b f"
+  "d e f"
+  "d e f"
+  "d h i"
+  "g h i"
+  "g h j"
+  "g h j"
+`;
+const gridTemplateSmallScreens = `
+  "a"
+  "a"
+  "a"
+  "a"
+  "b"
+  "b"
+  "b"
+  "b"
+  "c"
+  "c"
+  "c"
+  "d"
+  "d"
+  "d"
+  "e"
+  "e"
+  "f"
+  "f"
+  "f"
+  "g"
+  "g"
+  "g"
+  "h"
+  "h"
+  "h"
+  "h"
+  "i"
+  "i"
+  "j"
+  "j"
+`;
 
-const gridTemplate = `
-    "a b c"
-    "a b c"
-    "a b c"
-    "a b f"
-    "d e f"
-    "d e f"
-    "d h i"
-    "g h i"
-    "g h j"
-    "g h j"
-`
+const Dashboard = () => {
+  const isAboveMediumScreens = useMediaQuery("(min-width: 1200px)");
+  return (
+    <Box
+      width="100%"
+      height="100%"
+      display="grid"
+      gap="1.5rem"
+      sx={
+        isAboveMediumScreens
+          ? {
+              gridTemplateColumns: "repeat(3, minmax(370px, 1fr))",
+              gridTemplateRows: "repeat(10, minmax(60px, 1fr))",
+              gridTemplateAreas: gridTemplateLargeScreens,
+            }
+          : {
+              gridAutoColumns: "1fr",
+              gridAutoRows: "80px",
+              gridTemplateAreas: gridTemplateSmallScreens,
+            }
+      }
+    >
+      <Row1 />
+      <Row2 />
+      <Row3 />
+    </Box>
+  );
+};
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const Dashboard = (props: Props) => {
-    // const { palette } = useTheme();
-    return (
-        <Box width="100%" height="100%" display="grid" gap="1.5rem"
-            sx={{
-                gridTemplateColumns: "repeat(3, minmax(370px, 1fr))",
-                gridTemplateRows: "repeat(10, minmax(60px, 1fr))",
-                gridTemplateAreas: gridTemplate,
-            }}
-        >
-            <Box bgcolor="#fff" gridArea="a"></Box>
-            <Box bgcolor="#fff" gridArea="b"></Box>
-            <Box bgcolor="#fff" gridArea="c"></Box>
-            <Box bgcolor="#fff" gridArea="d"></Box>
-            <Box bgcolor="#fff" gridArea="e"></Box>
-            <Box bgcolor="#fff" gridArea="f"></Box>
-            <Box bgcolor="#fff" gridArea="g"></Box>
-            <Box bgcolor="#fff" gridArea="h"></Box>
-            <Box bgcolor="#fff" gridArea="i"></Box>
-            <Box bgcolor="#fff" gridArea="j"></Box>
-        </Box>
-    )
-}
-
-export default Dashboard
+export default Dashboard;
